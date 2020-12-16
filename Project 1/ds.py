40bc4c01a832d5535bb4346684f320c4af6e76d4
danial <danial.kordmodanloou@gmail.com>
2020-12-16 13:43:04 +0330

    new function for representing stored data, some changes in saving in heap

--- Project 1/ds.py
+++ Project 1/ds.py
@@ -1,12 +1,15 @@
+import copy
+
 class Card:
-    def __init__(self):
-        self.color = "" #R,G or B
-        self.number = 1
-    def setValues(self,color,number):
+    def __init__(self,color,number):
+         #R,G or B
         self.color = color
         self.number = number
 
-class Node:
+    def getCardData(self):
+        return ""+self.color+ str(self.number)
+
+class Placement:
     def __init__(self):
         self.contain = []
         self.length = 0
@@ -28,9 +31,52 @@ class Node:
             else:
                 return False
 
+    def isNotEmpty(self):
+        return self.length > 0
+
     def seeLastCard(self):
         return self.contain[self.length-1]
 
     def popLastCard(self):
         self.length-=1
         return self.contain.pop(self.length)
+    
+    def finalState(self,maxLength):
+        if(self.length == maxLength):
+            for i in range(1,maxLength):
+                if self.contain[i-1].color != self.contain[i].color:
+                    return False
+            return True
+        else:
+            return False
+
+    def placementData(self):
+        t = ""
+        for i in self.contain:
+            t += i.getCardData() + " "
+        return t
+
+class Node:
+    def __init__(self,length,array):
+        # self.placements = []
+        # for i in range(length):
+        #     elemnt = array[i]
+        #     for
+        #     p = Placement()
+        #     p.addCard(.contain[i])
+        #     self.placements.append(p)
+        self.placements = copy.deepcopy(array)
+        self.length = length
+
+    def changeCardPlace(self,i,j):
+        if self.placements[i].isNotEmpty():
+            if self.placements[j].addCard(self.placements[i].seeLastCard()):
+                self.placements[i].popLastCard()
+                return True
+        else:
+            return False
+
+    def currentNodeState(self):
+        for i in range(self.length):
+            print("K"+ str(i+1) + ":")
+            print(self.placements[i].placementData())
--- Project 1/p1.py
+++ Project 1/p1.py
@@ -3,11 +3,39 @@ import ds
 frontier = []
 explored = []
 
-c = ds.Node()
-card = ds.Card()
-card.setValues("R",1)
+place1 = ds.Placement()
+card1 = ds.Card("R",1)
+card2 = ds.Card("G",2)
+place1.addCard(card1,True)
+place1.addCard(card2,True)
 
-c.addCard(card,True)
+print(place1.placementData())
 
-print(c.seeLastCard().number)
+place2 = ds.Placement()
+card12 = ds.Card("G",3)
+card22 = ds.Card("R",4)
+place2.addCard(card12,True)
+place2.addCard(card22,True)
+
+print(place2.placementData())
+
+places = [place1,place2]
+
+
+root2 = ds.Node(2,places)
+
+root = ds.Node(2,places)
+root.currentNodeState()
+
+root.changeCardPlace(0,1)
+
+root.currentNodeState()
+
+print("  ")
+
+
+root2.currentNodeState()
+
+
+# will use the "befire node sxpandation condition" for optimization purposes
 
